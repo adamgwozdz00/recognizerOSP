bfb471d01cded3cecc933bb6e151be5f00a1e251
revinto <jakub.gornikk@gmail.com>
2022-06-07 19:52:22 +0200

    Uplaod image button

--- src/components/Recegonizer.vue
+++ src/components/Recegonizer.vue
@@ -12,7 +12,14 @@
       <!-- clearfix -->
     </div>
     <div class="flex-1 p-10 flex justify-center items-center h-full">
-      <div class="h-full max-h-[500px] mx-auto aspect-square rounded-xl border-2 border-solid border-white/50"></div>
+      <div class="h-full max-h-[500px] mx-auto aspect-square rounded-xl border-2 border-solid border-white/50 justify-center flex items-center" >
+       <div 
+    :class="{ 'opacity-0': !recegonizerExpanded }"
+    @click="collapse()">
+    <input type="file" id="file" accept="image/*">
+    <label for="file" class="text-white px-5 py-2.5 bg-gradient-to-r from-[#3B0057] to-[#6D0000] uppercase rounded-full mt-5 cursor-pointer">Choose photo</label>
+    </div>
+      </div>
     </div>
     <div class="flex basis-[250px] py-5">
       <div
